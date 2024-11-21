fd82884bbc06e0a5817dc3cc1bf180732d79a995
k45wu <k45wu@uwaterloo.ca>
2024-11-21 18:50:10 -0500

    added Observer pattern

---
+++ subject.cc
@@ -0,0 +1,10 @@
+#include "subject.h"
+
+void Subject::attach (Observer *o) {
+}
+
+void Subject::detach(Observer *o) {
+}
+
+void Subject::notifyObservers() {}
+
--- subject.h
+++ subject.h
@@ -1,8 +1,18 @@
 #ifndef __SUBJECT__
 #define __SUBJECT__
+#include "vector"
+#include "observer.h"
+
 
 class Subject {
-};
+  std::vector<Observer*> observers;
 
+ public:
+  Subject();
+  void attach(Observer *o);
+  void detach(Observer *o);
+  void notifyObservers();
+  virtual ~Subject()=0;
+};
 
 #endif
