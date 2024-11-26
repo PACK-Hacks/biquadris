ffadc1f059f9a58ceca95f7ea0a92805b87f93ff
pjpatel <pjpatel@uwaterloo.ca>
2024-11-26 16:35:32 -0500

    Updated game.cc to merge code

---
+++ game.cc
@@ -0,0 +1,45 @@
+#include <memory>
+#include "display.h"
+#include <string>
+#include "game.h"
+#include <sstream>
+#include <iostream>
+#include "player.h"
+using namespace std;
+
+class Game {
+    Player p1;
+    Player p2;
+    bool text;
+    int seed;
+    string scriptfile1;
+    string scriptfile2;
+    int startlevel;
+
+    public:
+    Game(bool text, int seed, string scriptfile1, string scriptfile2, int startlevel = 0): text{text}, seed{seed},
+    scriptfile1{scriptfile1}, scriptfile2{scriptfile2}, startLevel{startLevel}, 
+    p1{text, seed, scriptfile1, startlevel, this}, p2{text, seed, scriptfile2, startlevel, this} {}
+    // Remember to onstruct players with next block in player constructor
+
+    Player runGame() {
+        string special;
+
+
+        while (true) {
+            special = p1.runTurn(special);
+            if (p1.getLost() == true) return p2;
+            special = p2.runTurn(special);
+            if (p2.getLost() == true) return p1;
+        }
+
+        return p1;
+
+    }
+
+    void restart() {
+        p1.reset(); // Implement in display
+        p2.reset();
+    }
+
+};
---
+++ game.h
@@ -0,0 +1,12 @@
+class Game {
+    Player p1;
+    Player p2;
+
+    public:
+    Game(bool text, int seed, string scriptfile1, string scriptfile2, int startlevel);
+    
+    // Remember to onstruct players with next block in player constructor
+
+    Player runGame() {}
+
+};
